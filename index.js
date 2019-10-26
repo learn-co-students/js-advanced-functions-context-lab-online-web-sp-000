4cf9df07926c28738b9b2904e7ca2e79abb8c8ec
abogailo <abagailanne@gmail.com>
2019-10-26 17:20:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,59 @@
 /* Your Code Here */
+function createEmployeeRecord(employee) {
+  return {
+      firstName: employee[0],
+      familyName: employee[1],
+      title: employee[2],
+      payPerHour: employee[3],
+      timeInEvents: [],
+      timeOutEvents: []
+  };
+};
+
+function createEmployeeRecords(employees) {
+  return employees.map(employee => createEmployeeRecord(employee));
+};
+
+function createTimeInEvent(dateTime) {
+    const [date, hour] = dateTime.split(' ')
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour), 
+        date
+    })
+    return this;
+};
+
+function createTimeOutEvent(dateTime) {
+    const [date, hour] = dateTime.split(' ')
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour),
+        date
+    })
+
+    return this;
+};
+
+function hoursWorkedOnDate(date) {
+  return (((this.timeOutEvents.find(event => event.date === date).hour) 
+  - (this.timeInEvents.find(event => event.date === date).hour)))/100;
+};
+
+function wagesEarnedOnDate(date){
+  return hoursWorkedOnDate.call(this, date) * this.payPerHour;
+};
+
+function calculatePayroll(employees) {
+  return employees.reduce((total, employee) => total += allWagesFor.call(employee), 0);
+};
+
+function findEmployeeByFirstName(employees, firstNameStr) {
+  return employees.find(employee => employee.firstName === firstNameStr);
+};
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage

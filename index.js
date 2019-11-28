7f5a2c9c73178b0ce88022f16a0363319585bbcf
Bry Cox <carl.cc@gmail.com>
2019-11-28 11:17:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,56 @@
 /* Your Code Here */
 
+function createEmployeeRecord(employee) {
+  return {
+      firstName: employee[0],
+      familyName: employee[1],
+      title: employee[2],
+      payPerHour: employee[3],
+      timeInEvents: [],
+      timeOutEvents: []
+  };
+};
+
+function createEmployeeRecords(employees) {
+  return employees.map(employee => createEmployeeRecord(employee));
+};
+
+function createTimeInEvent(timeIn) {
+  const dateTime = timeIn.split(' ');
+  this.timeInEvents.push({
+    type: "TimeIn",
+    date: dateTime[0],
+    hour: parseInt(dateTime[1], 10)
+  });
+  return this;
+};
+
+function createTimeOutEvent(timeOut) {
+  const dateTime = timeOut.split(' ');
+  this.timeOutEvents.push({
+    type: "TimeOut",
+    date: dateTime[0],
+    hour: parseInt(dateTime[1], 10)
+  });
+  return this;
+};
+
+function hoursWorkedOnDate(date) {
+  return (this.timeOutEvents.find(event => event.date === date).hour - this.timeInEvents.find(event => event.date === date).hour)/100;
+};
+
+function wagesEarnedOnDate(date) {
+  return hoursWorkedOnDate.call(this, date) * this.payPerHour;
+};
+
+function calculatePayroll(employees) {
+  return employees.reduce((total, employee) => total += allWagesFor.call(employee), 0);
+};
+
+function findEmployeeByFirstName(employees, firstName) {
+  return employees.find(employee => employee.firstName === firstName);
+};
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but

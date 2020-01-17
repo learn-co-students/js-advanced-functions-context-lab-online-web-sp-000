6820a91d74ccd05a85ec4bee5a579d5a34ec27df
ericabasak <ericabasak@gmail.com>
2020-01-17 00:10:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,74 @@
 /* Your Code Here */
 
+function createEmployeeRecord(arr) {
+    let obj = {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return obj;
+}
+
+function createEmployeeRecords(er) {
+    let array = []
+    for (let i = 0; i < er.length; i++) {
+        array.push(createEmployeeRecord(er[i]))
+    }
+    return array;
+}
+
+
+
+function createTimeInEvent(ds) {
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(ds.split(" ")[1]),
+        date: ds.split(" ")[0]
+    })
+    return this;
+}
+
+
+function createTimeOutEvent(ds) {
+    this.timeOutEvents.push({
+        type:"TimeOut",
+        hour: parseInt(ds.split(" ")[1]),
+        date: ds.split(" ")[0]
+    })
+    return this;
+}
+
+
+function hoursWorkedOnDate(dateForm) {
+    let timeIn = this.timeInEvents.find(obj => obj.date === dateForm);
+    let timeOut = this.timeOutEvents.find(obj => obj.date === dateForm);
+    return (timeOut.hour - timeIn.hour) / 100;
+}
+
+
+
+function wagesEarnedOnDate(dateForm) {
+    return hoursWorkedOnDate.call(this, dateForm) * this.payPerHour;
+}
+
+
+
+function calculatePayroll(empRec) {
+    return empRec.reduce(function(total, cv) { return total + allWagesFor.call(cv)}, 0);
+}
+
+
+
+function findEmployeeByFirstName(srcArray, firstName) {
+   return srcArray.find(function(name) {
+       return name.firstName === firstName;
+   })
+}
+
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but

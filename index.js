cbdf75455be335a25f826f68f17a0fd828c1138b
Ronald Stephens <rstephens@bluemorpho.cc>
2020-04-09 19:29:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,57 @@
 /* Your Code Here */
+let createEmployeeRecord = function(row) {
+    return {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+let createEmployeeRecords = function(employeeRowData) {
+    return employeeRowData.map(function(row) {
+        return createEmployeeRecord(row)
+    })
+}
+
+let createTimeInEvent = function(dateStamp) {
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+    return this
+}
+
+let createTimeOutEvent = function(dateStamp) {
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+    return this
+}
+
+let hoursWorkedOnDate = function(specificDate) {
+    let inEvent = this.timeInEvents.find(function(e) {
+        return e.date === specificDate
+    })
+    let outEvent = this.timeOutEvents.find(function(e) {
+        return e.date === specificDate
+    })
+    console.log((outEvent.hour - inEvent.hour) /100)
+    return (outEvent.hour - inEvent.hour) / 100
+}
+
+let wagesEarnedOnDate = function(specificDate) {
+    return hoursWorkedOnDate(specificDate, this) * this.payPerHour;
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage

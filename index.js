5cb1d0862ed05c551d5218ecefffb8de1bf8985d
zdsmit <zdsmith001@gmail.com>
2020-05-18 11:26:41 -0700

    Done.

--- index.js
+++ index.js
@@ -9,14 +9,71 @@
  for you to use if you need it!
  */
 
+let createEmployeeRecord = function(record) {
+  return {
+    firstName: record[0],
+    familyName: record[1],
+    title: record[2],
+    payPerHour: record[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  }
+}
+
+let createEmployeeRecords = function(records) {
+  return records.map(e => createEmployeeRecord(e))
+}
+
+let createTimeInEvent = function(datetime) {
+  let time = datetime.toString().split(" ")
+  let event = {
+    type: "TimeIn",
+    hour: parseInt(time[1]),
+    date: time[0]
+  }
+  this.timeInEvents.push(event)
+  return this
+}
+
+let createTimeOutEvent = function(datetime) {
+  let time = datetime.toString().split(" ")
+  let event = {
+    type: "TimeOut",
+    hour: parseInt(time[1]),
+    date: time[0]
+  }
+  this.timeOutEvents.push(event)
+  return this
+}
+
+let hoursWorkedOnDate = function(date) {
+  let timeIn = this.timeInEvents.find(e => e.date === date)
+  let timeOut = this.timeOutEvents.find(e => e.date === date)
+  return (timeOut.hour - timeIn.hour)/100
+}
+
+let wagesEarnedOnDate = function(date) {
+  return hoursWorkedOnDate.call(this, date) * this.payPerHour
+}
+
 let allWagesFor = function () {
-    let eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
+  let eligibleDates = this.timeInEvents.map(function (e) {
+    return e.date
+  })
 
-    let payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+  let payable = eligibleDates.reduce(function (memo, d) {
+    return memo + wagesEarnedOnDate.call(this, d)
+  }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
-    return payable
+  return payable
+}
+
+let findEmployeeByFirstName = function(records, name) {
+  return records.find(e => e.firstName === name)
+}
+
+let calculatePayroll = function(records) {
+  return records.reduce(function(total, employee) {
+    return allWagesFor.call(employee) + total
+  }, 0)
 }

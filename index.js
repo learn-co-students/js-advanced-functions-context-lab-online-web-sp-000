42b3b05d832b5441413652e6b493e752d4c79287
SLarkworthy <Sarah.Larkworthy@gmail.com>
2020-06-03 14:07:59 -0400

    Done.

--- index.js
+++ index.js
@@ -20,3 +20,58 @@ let allWagesFor = function () {
 
     return payable
 }
+
+function createEmployeeRecord(arr) {
+    return {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+}
+
+function createEmployeeRecords(arr) {
+    return arr.map(employee => createEmployeeRecord(employee));
+}
+
+function createTimeInEvent(date) {
+    this.timeInEvents.push({
+        type: "TimeIn", 
+        hour: parseInt(date.split(' ')[1]),
+        date: date.split(' ')[0]
+    })
+    return this;
+}
+
+function createTimeOutEvent(date) {
+    this.timeOutEvents.push({
+        type: "TimeOut", 
+        hour: parseInt(date.split(' ')[1]),
+        date: date.split(' ')[0]
+    })
+    return this;
+}
+
+function hoursWorkedOnDate(date) {
+    let inEvent = this.timeInEvents.find(element => element.date === date);
+    let outEvent = this.timeOutEvents.find(element => element.date === date);
+
+    return (outEvent.hour - inEvent.hour) / 100;
+}
+
+
+function wagesEarnedOnDate(date) {
+    return hoursWorkedOnDate.call(this, date) * parseInt(this.payPerHour);
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(employee => employee.firstName === firstName);
+}
+
+function calculatePayroll(array) {
+    return array.reduce(function(accumulator, employee) {
+        return accumulator + allWagesFor.call(employee);
+    }, 0);
+}

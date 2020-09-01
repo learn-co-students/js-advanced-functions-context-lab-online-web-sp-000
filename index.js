fbe3132d2dd22bab70bd8b5222787116ee7dfd3c
tibraj <tibraj94@gmail.com>
2020-09-01 13:12:12 -0400

    Done.

--- index.js
+++ index.js
@@ -20,3 +20,68 @@ let allWagesFor = function () {
 
     return payable
 }
+
+let createEmployeeRecord = function(row) {
+    return {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+};
+
+let createEmployeeRecords = function(employeeRow) {
+    return employeeRow.map(function(row){
+        return createEmployeeRecord(row)
+    })
+};
+
+let createTimeInEvent = function(stamp) {
+    let [date, hour] = stamp.split(' ')
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date    
+    })
+    return this;
+};
+
+let createTimeOutEvent = function(stamp) {
+    let [date, hour] = stamp.split(' ')
+    this.timeInEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date    
+    })
+    return this;
+};
+
+let hoursWorkedOnDate = function(onDate) {
+    let inEvent = this.timeInEvents.find(function(e){
+        return e.date === onDate
+    });
+    let outEvent = object.timeOutEvents.find(function(e){
+        return e.date === onDate
+    });
+    return (outEvent.hour - inEvent.hour)/100;
+};
+
+let wagesEarnedOnDate = function(onDate) {
+    let wage = hoursWorkedOnDate.call(this, onDate) * this.payPerHour;
+    return parseFloat(wage.toString())
+};
+
+
+let findEmployeeByFirstName = function(array, firstName) {
+    return array.find(function(employee){
+        return employee.firstName === firstName);
+    }
+};
+
+let calculatePayroll = function(array) {
+    return array.reduce(function(accumulator, employee) {
+        return accumulator + allWagesFor.call(employee);
+    }, 0);
+};

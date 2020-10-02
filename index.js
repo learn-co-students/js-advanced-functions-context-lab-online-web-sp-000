673eef82f0962d5b5eebb5f6064c7df803abe822
rachavez1974 <rachavez1974@gmail.com>
2020-10-02 10:35:48 -0600

    Done.

--- index.js
+++ index.js
@@ -20,3 +20,63 @@ let allWagesFor = function () {
 
     return payable
 }
+
+// Your code here
+function createEmployeeRecord(empInfo){
+  return {
+    firstName: empInfo[0],
+    familyName: empInfo[1],
+    title: empInfo[2],
+    payPerHour: empInfo[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  }
+}
+
+function createEmployeeRecords(empsInfo){
+  return empsInfo.map( empInfo => createEmployeeRecord(empInfo))
+}
+
+let createTimeInEvent = function(){
+  console.log(this)
+  console.log(this.timeInEvents)
+  console.log(arguments[0])
+  let timeInEvent = this.timeInEvents.push(
+    {
+      type: "TimeIn",
+      hour: parseInt(arguments[0].split(' ')[1]),
+      date: arguments[0].split(' ')[0]
+    })
+  // console.log(timeInEvent)
+}
+
+function createTimeOutEvent(empRecord, date){
+  empRecord.timeOutEvents.push(
+  {
+    type: "TimeOut",
+    hour: parseInt(date.split(' ')[1]),
+    date: date.split(' ')[0]
+  })
+  return empRecord
+}
+
+function hoursWorkedOnDate(empRecord, date){
+  return (parseInt(empRecord.timeOutEvents.find(timeIn => timeIn.date == date).hour) - parseInt(empRecord.timeInEvents.find(timeIn => timeIn.date == date).hour))/100
+}
+
+function wagesEarnedOnDate(empRecord, date){
+  return hoursWorkedOnDate(empRecord, date) * empRecord.payPerHour
+}
+
+// function allWagesFor(empRecord){
+//    return empRecord.timeInEvents.map(day => day.date).reduce(function(memo, d) { return memo + wagesEarnedOnDate(empRecord, d)}, 0)
+// }
+
+function findEmployeeByFirstName(employees, firstName){
+  return employees.find(employee => employee.firstName == firstName)
+}
+
+function calculatePayroll(employees){
+  return employees.reduce( function(memo, employee){ return memo + allWagesFor(employee)}, 0)
+}
+

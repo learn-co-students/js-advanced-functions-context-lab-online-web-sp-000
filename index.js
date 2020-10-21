3f8790cc46196d5247442dc5204f26ba0497e658
jessicagomesng <jgnpeluso@gmail.com>
2020-10-21 16:06:24 +0100

    Done.

--- index.js
+++ index.js
@@ -8,6 +8,48 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
+function createEmployeeRecord(infoArray) {
+   return Object.assign({}, 
+    { firstName: infoArray[0] }, 
+    { familyName: infoArray[1] }, 
+    { title: infoArray[2] }, 
+    { payPerHour: infoArray[3] }, 
+    { timeInEvents: [] }, 
+    { timeOutEvents: [] } );
+}
+
+function createEmployeeRecords(array) {
+    return array.map(employee => createEmployeeRecord(employee));
+}
+
+function createTimeInEvent(dateStamp) {
+    let clockIn = Object.assign({}, { type: 'TimeIn' }, { hour: parseInt(dateStamp.split(' ')[1]) }, { date: dateStamp.split(' ')[0] })
+    this.timeInEvents.push(clockIn);
+    return this;
+}
+
+function createTimeOutEvent(dateStamp) {
+    let clockOut = Object.assign({}, { type: 'TimeOut' }, { hour: parseInt(dateStamp.split(' ')[1]) }, { date: dateStamp.split(' ')[0] })
+    this.timeOutEvents.push(clockOut);
+    return this;
+}
+
+function hoursWorkedOnDate(date) {
+    let clockIn = this.timeInEvents.find(event => event.date === date).hour
+    let clockOut = this.timeOutEvents.find(event => event.date === date).hour
+
+    return (clockOut - clockIn) / 100;
+}
+
+function wagesEarnedOnDate(date) {
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour;
+}
+
+function calculatePayroll(array) {
+    // add all wages for each employee of the array
+    let reducer = (acc, employee) => acc + allWagesFor.call(employee)
+    return array.reduce(reducer, 0);
+}
 
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
@@ -19,4 +61,8 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(employee => employee.firstName === firstName);
 }

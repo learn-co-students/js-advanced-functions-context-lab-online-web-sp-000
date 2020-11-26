3e9f9cc4deed6a05c3e1aab7aadef5b58a72d589
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-11-26 03:01:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 /* Your Code Here */
+let createEmployeeRecord = function(row) {
+  return {
+    firstName: row[0],
+    familyName: row[1],
+    title: row[2],
+    payPerHour: row[3],
+    timeInEvents: [],
+    timeOutEvents: [],
+  }
+}
 
+let createEmployeeRecords = function(employeeRowData){
+  return employeeRowData.map( function(row){
+    return createEmployeeRecord(row)
+  })
+}
+
+let createTimeInEvent = function(employee, dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+
+    employee.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return employee
+}
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but

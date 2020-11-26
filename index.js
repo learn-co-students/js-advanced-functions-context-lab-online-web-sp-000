48cc64cab72e460d16fa014d9ad3e885e58b58fe
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-11-26 03:24:35 +0000

    Done.

--- index.js
+++ index.js
@@ -16,16 +16,48 @@ let createEmployeeRecords = function(employeeRowData){
   })
 }
 
-let createTimeInEvent = function(employee, dateStamp){
+//reminder that we are using this as our name - an explicit override
+let createTimeInEvent = function(dateStamp){
     let [date, hour] = dateStamp.split(' ')
 
-    employee.timeInEvents.push({
+    this.timeInEvents.push({
         type: "TimeIn",
         hour: parseInt(hour, 10),
         date,
     })
 
-    return employee
+    return this
+}
+
+let createTimeOutEvent = function(dateStamp){
+  let [date, hour] = dateStamp.split(' ')
+
+  this.timeOutEvents.push({
+    type: "TimeOut",
+    hour: parseInt(hour, 10),
+    date,
+  })
+
+  return this
+}
+
+let hoursWorkedOnDate = function(soughtDate){
+  let clockIn = this.timeInEvents.find(function(e){
+    return e.date === soughtDate
+  })
+
+  let clockOut = this.timeOutEvents.find(function(e){
+    return e.date === soughtDate
+  })
+
+  return (clockOut.hour - clockIn.hour)/100
+}
+
+//reminder parseFloat() returns a value that will convert into a string and
+//return a floating point number
+let wagesEarnedOnDate = function(dateSought){
+  let rawWage = hoursWorkedOnDate.call(this, dateSought) * this.payPerHour
+  return parseFloat(rawWage)
 }
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -46,4 +78,16 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+let calculatePayroll = function(arrayOfEmployees){
+  return arrayOfEmployees.reduce(function(memo, rec){
+    return memo + allWagesFor(rec)
+  }, 0)
+}
+
+let findEmployeeByFirstName = function(srcArray, firstName){
+  return srcArray.find(function(rec){
+    return rec.firstName === firstName
+  })
 }

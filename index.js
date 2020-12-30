b51daac559a9f7725600a258a64d1e03f2092dd5
CoderJay06 <jaycru06@yahoo.com>
2020-12-30 14:12:41 -0500

    Done.

--- index.js
+++ index.js
@@ -9,14 +9,32 @@
  for you to use if you need it!
  */
 
-let allWagesFor = function () {
-    let eligibleDates = this.timeInEvents.map(function (e) {
+// createEmployeeRecord
+// 2) populates a firstName field from the 0 th element
+// 3) populates a familyName field from the 1 th element
+// 4) populates a title field from the 2 th element
+// 5) populates a payPerHour field from the 3 th element
+// 6) initializes a field, timeInEvents, to hold an empty Array
+// 7) initializes a field, timeOutEvents, to hold an empty Array
+const createEmployeeRecord = (employee) => {
+    return {
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+let allWagesFor = function() {
+    let eligibleDates = this.timeInEvents.map(function(e) {
         return e.date
     })
 
-    let payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+    let payable = eligibleDates.reduce(function(memo, d) {
+            return memo + wagesEarnedOnDate.call(this, d)
+        }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
 }

df045b36374d23c56176e0c564f14096e169c403
clincoln12 <chris.lincoln12@gmail.com>
2021-01-14 21:09:15 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,66 @@
 /* Your Code Here */
 
+function createEmployeeRecord(arr){
+    let record
+    return record = {
+        firstName: arr[0],
+        familyName: arr[1], 
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+
+    }
+}
+
+function createEmployeeRecords(arrs) {
+    let records = arrs.map(arr => createEmployeeRecord(arr))
+    return records
+}
+
+function createTimeInEvent(dateStamp) {
+    let date = dateStamp.split(' ');
+    this.timeInEvents.push({
+      type: 'TimeIn',
+      hour: parseInt(date[1]),
+      date: date[0]
+    });
+    return this;
+  }
+
+function createTimeOutEvent(dateStamp) {
+    let date = dateStamp.split(' ');
+    this.timeOutEvents.push({
+        type: 'TimeOut',
+        hour: parseInt(date[1]),
+        date: date[0]
+    });
+    return this;
+}
+
+function hoursWorkedOnDate(date) {
+    let timeIn = this.timeInEvents.find(timeEvent => timeEvent.date === date);
+    let timeOut = this.timeOutEvents.find(timeEvent => timeEvent.date === date);
+    return (timeOut.hour - timeIn.hour)/100;
+}
+
+function wagesEarnedOnDate(date) {
+    let wages = hoursWorkedOnDate.call(this, date) * this.payPerHour
+    return wages
+}
+
+function calculatePayroll(arrs){
+
+    let payroll = arrs.reduce((total, emp) => {return total + allWagesFor.call(emp)}, 0) //removed 'this' from arguments as the allWagesFor method 'bind'ed 'this' as the execution context
+    return payroll
+
+}
+
+function findEmployeeByFirstName(srcArray, firstName){
+    let name = srcArray.find(r => r.firstName === firstName)
+    return name
+}
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but

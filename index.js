b325bda20ff5a35367229a90e9b0cd7ef5741615
William Pannell <lpann5@gmail.com>
2021-02-09 16:19:14 -0600

    Done.

--- index.js
+++ index.js
@@ -8,6 +8,59 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
+function createEmployeeRecord(arr){
+    let employeeObj = {
+        firstName : arr[0],
+        familyName : arr[1],
+        title : arr[2],
+        payPerHour :arr[3],
+        timeInEvents : [],
+        timeOutEvents : []
+    }
+    return employeeObj
+}
+
+function createEmployeeRecords(arrArr){
+    return arrArr.map(function(emp){
+        return createEmployeeRecord(emp)
+    });
+}
+
+function createTimeInEvent(dateStamp){
+        let [d, h] = dateStamp.split(' ');
+        this.timeInEvents.push({
+            type: "TimeIn",
+            hour: parseInt(h, 10),
+            date: d
+        })
+        return this
+}
+
+function createTimeOutEvent(dateStamp){
+    let [d, h] = dateStamp.split(' ');
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(h, 10),
+        date: d
+    })
+    return this
+}
+
+function hoursWorkedOnDate(givenDate){
+    let inEvent = this.timeInEvents.find(function(emp){
+        return emp.date === givenDate
+    })
+    let outEvent = this.timeOutEvents.find(function(emp){
+        return emp.date === givenDate
+    })
+    return (outEvent.hour - inEvent.hour) / 100
+}
+
+function wagesEarnedOnDate (givenDate){
+    let amount = hoursWorkedOnDate.call(this, givenDate)
+        * this.payPerHour
+    return parseFloat(amount.toString())
+}
 
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
@@ -19,4 +72,16 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function findEmployeeByFirstName(srcArray, firstName){
+    return srcArray.find(function(record){
+        return record.firstName === firstName
+    })
+}
+
+function calculatePayroll (emprecArr){
+    return emprecArr.reduce(function(total, records){
+        return total + allWagesFor.call(records)
+    }, 0)
 }

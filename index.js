bd347da82a9b6017ef2843925d0a50041a9926c1
Chandra Lama <iamme24cl@gmail.com>
2021-03-06 22:37:46 -0800

    Done.

--- index.js
+++ index.js
@@ -8,6 +8,17 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
+function createEmployeeRecord(record) {
+    let employeeRecord = {
+        firstName: record[0],
+        familyName: record[1],
+        title: record[2],
+        payPerHour: record[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+    return employeeRecord;
+}
 
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {

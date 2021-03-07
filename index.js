2e0597197f2ebdf89c1dbe779bb2ae903dfa022c
Chandra Lama <iamme24cl@gmail.com>
2021-03-06 23:42:46 -0800

    Done.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
-function createEmployeeRecord(record) {
+let createEmployeeRecord = function(record) {
     let employeeRecord = {
         firstName: record[0],
         familyName: record[1],
@@ -18,6 +18,50 @@ function createEmployeeRecord(record) {
         timeOutEvents: []
     };
     return employeeRecord;
+}
+
+let createEmployeeRecords = function(records) {
+    return records.map(record => {
+        return createEmployeeRecord(record);
+    });
+}
+
+let createTimeInEvent = function(dateStamp) {
+    let [date, hour] = dateStamp.split(" ");
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date: date
+    });
+    return this;
+}
+
+let createTimeOutEvent = function(dateStamp) {
+    let [date, hour] = dateStamp.split(" ");
+    this.timeOutEvents.push ({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date: date
+    });
+    return this;
+}
+
+let hoursWorkedOnDate = function(date) {
+    let timeInEvent = this.timeInEvents.find(event => {
+       return event.date === date;
+    });
+    let timeOutEvent = this.timeOutEvents.find(event => {
+       return event.date === date;
+    });
+
+    return (timeOutEvent.hour - timeInEvent.hour) / 100;
+
+}
+
+let wagesEarnedOnDate = function(date) {
+    let wagesPayable = this.payPerHour * hoursWorkedOnDate.call(this, date);
+    return wagesPayable;
 }
 
 let allWagesFor = function () {
@@ -30,4 +74,16 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+let findEmployeeByFirstName = function(employeeRecords, firstName) {
+    return employeeRecords.find(record => {
+       return record.firstName === firstName;
+    });
+}
+
+let calculatePayroll = function(employeeRecords) {
+    return employeeRecords.reduce(function(accumulator, record) {
+        return accumulator + allWagesFor.call(record);
+    }, 0);
 }

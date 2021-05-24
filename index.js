874f292eb2b03d6d2387ad0c9c2942eb4bd26a83
kkirby16 <kkirby16@my.whitworth.edu>
2021-05-23 18:18:21 -0700

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,55 @@
 /* Your Code Here */
 
+function createEmployeeRecord(array) { 
+    return { 
+        firstName: array[0], 
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [], 
+        timeOutEvents: []
+        }
+}
+
+function createEmployeeRecords(array) { 
+    return array.map(function(val) { 
+        return createEmployeeRecord(val)
+    }
+    )}
+
+
+    function createTimeInEvent(date) { 
+        this.timeInEvents.push({
+            type: "TimeIn", 
+            hour: parseInt(date.split(" ")[1]),
+            date: date.split(" ")[0]
+        })
+    
+        return this
+
+    }
+
+    function createTimeOutEvent(date) { 
+        this.timeOutEvents.push({
+            type: "TimeOut",
+            hour: parseInt(date.split(" ")[1]),
+            date: date.split(" ")[0]
+        })
+        
+        return this 
+    }
+
+    function hoursWorkedOnDate(dateToSearch) { 
+        console.log(this)
+        let timeIn = this.timeInEvents.find(timeInEvent => timeInEvent.date === dateToSearch)
+
+        let timeOut = this.timeOutEvents.find(timeOutEvent => timeOutEvent.date === dateToSearch)
+
+        return (timeOut.hour - timeIn.hour) / 100
+
+    }
+
+  
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but

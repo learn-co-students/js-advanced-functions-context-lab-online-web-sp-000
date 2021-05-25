e028fb20c33f988dc1f9db520469ceec6e19879d
Sonja Parsell <hello@sparsell.com>
2021-05-25 13:27:41 -0400

    Done.

--- index.js
+++ index.js
@@ -20,3 +20,69 @@ let allWagesFor = function () {
 
     return payable
 }
+
+let createEmployeeRecord = function (employee) {
+    return {
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+let createEmployeeRecords = function(employees) {
+    return employees.map(function(line) {
+        return createEmployeeRecord(line)
+    })
+}
+
+let createTimeInEvent = function(dateStamp) {
+    let [date, hour] = dateStamp.split(' ')
+    
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10), 
+        date,
+    })
+    return this
+}
+
+let createTimeOutEvent = function(dateStamp) {
+    let [date, hour] = dateStamp.split(' ')
+    
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10), 
+        date,
+    })
+    return this
+}
+
+let hoursWorkedOnDate = function(dateStamp) {
+    let timeInOnDate = this.timeInEvents.find(function(e){
+        return e.date === dateStamp
+    })
+    let timeOutOnDate = this.timeOutEvents.find(function(e){
+        return e.date === dateStamp
+    })
+        return (timeOutOnDate.hour - timeInOnDate.hour) / 100
+}
+
+let wagesEarnedOnDate = function(dateStamp) {
+    let grossWage = hoursWorkedOnDate.call(this,dateStamp) * this.payPerHour
+    return parseFloat(grossWage.toString())
+}
+
+let calculatePayroll = function(employee) {
+    return employee.reduce(function(memo, rec){
+        return memo + allWagesFor.call(rec)
+    }, 0)
+}
+
+let findEmployeeByFirstName = function(srcArray, firstName) {
+    return srcArray.find(function(e) {
+        return e.firstName === firstName
+    })
+}

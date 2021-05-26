cf53bcd516e8f6be4ec21609f8c2d16b541343a9
bongofury <jtroutmanman@gmail.com>
2021-05-26 17:27:51 -0500

    Done.

--- index.js
+++ index.js
@@ -9,6 +9,51 @@
  for you to use if you need it!
  */
 
+function createEmployeeRecord(person) {
+    return {
+        firstName: person[0],
+        familyName: person[1],
+        title: person[2],
+        payPerHour: person[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(array) {
+    return array.map(record => createEmployeeRecord(record));
+}
+
+function createTimeInEvent(dateStamp) {
+    let [date, time] = dateStamp.split(" ");
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(time, 10),
+        date: date
+    });
+    return this;
+}
+
+function createTimeOutEvent(dateStamp) {
+    let [date, time] = dateStamp.split(" ");
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(time, 10),
+        date: date
+    });
+    return this;
+}
+
+function hoursWorkedOnDate(date) {
+    let timeIn = this.timeInEvents.find(day => day.date === date);
+    let timeOut = this.timeOutEvents.find(day => day.date === date);
+    return (timeOut.hour - timeIn.hour) / 100;
+}
+
+function wagesEarnedOnDate(date) {
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour;
+}
+
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -19,4 +64,13 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function findEmployeeByFirstName(records, name) {
+    return records.find(record => record.firstName === name);
+}
+
+function calculatePayroll(records) {
+    let wages = records.map(record => allWagesFor.call(record));
+    return wages.reduce((memo, wage) => memo += wage);
 }
